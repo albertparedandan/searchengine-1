41b0d8213f0fc1b7c5af303ba0638b5b3ec6ef1e
np0805 <nickypratama17@yahoo.com>
2020-05-12 22:09:44 +0700

    fix word not found

--- src/rundb.go
+++ src/rundb.go
@@ -11,7 +11,7 @@ import (
 func main() {
 	database.OpenAllDb()
 	fmt.Println(time.Now())
-	r := retrieval.RetrievalFunction("computer -covid19 -more")
+	r := retrieval.RetrievalFunction("nani widjaja")
 	fmt.Println(time.Now())
 	for k, v := range r {
 		fmt.Println("key ", k, "value", v.GetURL(), v.GetTitle(), v.GetPageRank())
